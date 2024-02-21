f77b226ce6a37fc85c97687df014cbc19eaa638c
Jeremy Kennedy <me@jeremyk.net>
2024-02-21 18:45:24 -0500

    nix: modularize

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -1,7 +1,10 @@
+# https://github.com/Misterio77/nix-starter-configs/blob/main/standard/nixos/configuration.nix
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
+  inputs,
+  outputs,
   config,
   pkgs,
   ...
@@ -9,23 +12,53 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    # Import home-manager's NixOS module
+    inputs.home-manager.nixosModules.home-manager
   ];
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "nodejs-16.20.2"
-    "electron-25.9.0"
-  ];
+  home-manager = {
+    extraSpecialArgs = {inherit inputs outputs;};
+    users = {
+      # Import your home-manager configuration
+      jeremy = import ../home-manager/home.nix;
+    };
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "nodejs-16.20.2"
+        "electron-25.9.0"
+      ];
+    };
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "JeremyDesktop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -78,9 +111,6 @@
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "jeremy";
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jeremy = {
@@ -204,8 +234,6 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   fileSystems."/mnt/tower/appdata" = {
     device = "192.168.1.240:/mnt/user/appdata";
     fsType = "nfs";
@@ -216,7 +244,6 @@
   };
 
   # Enable the OpenSSH daemon.
-
   services.openssh = {
     enable = true;
     settings = {
@@ -269,8 +296,6 @@
     } # ftp
   ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
