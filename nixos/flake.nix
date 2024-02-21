829dc899e399bd48b525302fab2409e4bcbfc16e
Jeremy Kennedy <me@jeremyk.net>
2024-02-21 16:50:22 -0500

    nix: add home manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,12 +5,21 @@
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     nixpkgs-unstable,
+    home-manager,
   }: let
     system = "x86_64-linux";
     overlay-unstable = final: prev: {
@@ -30,7 +39,20 @@
             pkgs,
             ...
           }: {nixpkgs.overlays = [overlay-unstable];})
+
           ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.jeremy = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
         ];
       };
     };
---
+++ nixos/skipped-pkgs.md
@@ -0,0 +1,48 @@
+# Skipped Packages
+
+## archives
+xz
+
+## utils
+jq # A lightweight and flexible command-line JSON processor
+yq-go # yaml processer https://github.com/mikefarah/yq
+
+## networking tools
+mtr # A network diagnostic tool
+iperf3
+dnsutils  # `dig` + `nslookup`
+ldns # replacement of `dig`, it provide the command `drill`
+aria2 # A lightweight multi-protocol & multi-source command-line download utility
+socat # replacement of openbsd-netcat
+nmap # A utility for network discovery and security auditing
+ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+## nix related
+<!-- it provides the command `nom` works just like `nix` -->
+<!-- with more details log output -->
+nix-output-monitor
+
+## productivity
+hugo # static site generator
+
+## system call monitoring
+strace # system call monitoring
+ltrace # library call monitoring
+lsof # list open files
+
+## system tools
+sysstat
+lm_sensors # for `sensors` command
+ethtool
+pciutils # lspci
+usbutils # lsusb
+
+# misc
+file
+which
+tree
+gnused
+gnutar
+gawk
+zstd
+gnupg
