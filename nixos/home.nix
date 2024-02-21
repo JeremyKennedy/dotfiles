2e0b1af298e6c37933b79ef6855bebaeb07ec270
Jeremy Kennedy <me@jeremyk.net>
2024-02-21 17:20:12 -0500

    nix: neovim

--- nixos/home.nix
+++ nixos/home.nix
@@ -95,6 +95,12 @@
     ];
   };
 
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
   # alacritty - a cross-platform, GPU-accelerated terminal emulator
   # programs.alacritty = {
   #   enable = true;
