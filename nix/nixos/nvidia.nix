9416998b81750af040a017ee757421719c103fb4
Jeremy Kennedy <me@jeremyk.net>
2024-02-21 19:31:33 -0500

    nix: modularize nvidia

---
+++ nix/nixos/nvidia.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  hardware.nvidia = {
+    # Modesetting is needed most of the time
+    modesetting.enable = true;
+
+    # Enable power management (do not disable this unless you have a reason to).
+    # Likely to cause problems on laptops and with screen tearing if disabled.
+    powerManagement.enable = true;
+
+    # Use the NVidia open source kernel module (which isn't “nouveau”).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+    # force old nvidia
+    #package = (config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
+    # src = pkgs.fetchurl {
+    #   url = "https://download.nvidia.com/XFree86/Linux-x86_64/525.125.06/NVIDIA-Linux-x86_64-525.125.06.run";
+    #   sha256 = "17av8nvxzn5af3x6y8vy5g6zbwg21s7sq5vpa1xc6cx8yj4mc9xm";
+    # };
+    #});
+    #package = (config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
+    #  src = pkgs.fetchurl {
+    #    url = "https://download.nvidia.com/XFree86/Linux-x86_64/535.104.05/NVIDIA-Linux-x86_64-535.104.05.run";
+    #    sha256 = "1jgdrczy70jlg9ybi1z8mm9q5vg78rf66dknfppbww573nfn179g";
+    #  };
+    #});
+  };
+}
