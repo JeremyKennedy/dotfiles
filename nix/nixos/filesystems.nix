0862d1cc4becdb85455ccf8c944c74d692ae5368
Jeremy Kennedy <me@jeremyk.net>
2024-02-26 15:42:33 -0500

    nix.filesystems: add noatime, add media and temp

--- nix/nixos/filesystems.nix
+++ nix/nixos/filesystems.nix
@@ -1,14 +1,28 @@
+# https://nixos.wiki/wiki/NFS
 {
   fileSystems."/mnt/tower/appdata" = {
     device = "192.168.1.240:/mnt/user/appdata";
     fsType = "nfs";
+    options = ["noatime"];
   };
   fileSystems."/mnt/tower/general" = {
     device = "192.168.1.240:/mnt/user/general";
     fsType = "nfs";
+    options = ["noatime"];
   };
   fileSystems."/mnt/tower/backups" = {
     device = "192.168.1.240:/mnt/user/backups";
     fsType = "nfs";
+    options = ["noatime"];
+  };
+  fileSystems."/mnt/tower/temp" = {
+    device = "192.168.1.240:/mnt/user/temp";
+    fsType = "nfs";
+    options = ["noatime" "x-systemd.automount" "noauto"];
+  };
+  fileSystems."/mnt/tower/media" = {
+    device = "192.168.1.240:/mnt/user/media";
+    fsType = "nfs";
+    options = ["noatime" "x-systemd.automount" "noauto"];
   };
 }
