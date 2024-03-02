381a9b7f137ee08f835c9a92d48796b555539602
Jeremy Kennedy <me@jeremyk.net>
2024-03-01 19:04:56 -0500

    nix: add stable overlay

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -36,6 +36,7 @@
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
+      outputs.overlays.stable-packages
       outputs.overlays.unstable-packages
       outputs.overlays.master-packages
 
