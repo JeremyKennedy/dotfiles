7006c921b4e1d17137bf85a15b4b96bc8857a95f
Jeremy Kennedy <me@jeremyk.net>
2024-03-01 19:04:56 -0500

    nix: add stable overlay

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,8 +5,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
     home-manager = {
@@ -18,6 +18,7 @@
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-stable,
     nixpkgs-unstable,
     nixpkgs-master,
     home-manager,
