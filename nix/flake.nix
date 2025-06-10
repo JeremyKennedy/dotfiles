c4f25e9b59e5280866e61c8ea1fbc1631c8c6665
Jeremy Kennedy <me@jeremyk.net>
2025-06-09 22:15:00 -0400

    nix: update stable

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
     home-manager = {
