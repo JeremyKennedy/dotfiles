ff811a3b1075674449d063cf7b6b56ec0031777a
Jeremy Kennedy <me@jeremyk.net>
2025-07-15 22:40:54 -0400

    wofi/hyprland fixes

--- home-manager/programs.nix
+++ home-manager/programs.nix
@@ -11,4 +11,19 @@
     # programs to consider
     # firefox vscode
   };
+
+  # Hyprland configuration files
+  xdg.configFile = {
+    "hypr/hyprland.conf".source = ../modules/desktop/hyprland/configs/hypr/hyprland.conf;
+    "hypr/hypridle.conf".source = ../modules/desktop/hyprland/configs/hypr/hypridle.conf;
+    "hypr/hyprlock.conf".source = ../modules/desktop/hyprland/configs/hypr/hyprlock.conf;
+    
+    # Waybar configuration files
+    "waybar/config".source = ../modules/desktop/hyprland/configs/waybar/config;
+    "waybar/style.css".source = ../modules/desktop/hyprland/configs/waybar/style.css;
+    
+    # Wofi configuration files
+    "wofi/config".source = ../modules/desktop/hyprland/configs/wofi/config;
+    "wofi/style.css".source = ../modules/desktop/hyprland/configs/wofi/style.css;
+  };
 }
