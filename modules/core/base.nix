1b033080c0d614292550f6ddd2a39dd7e65b495b
Jeremy Kennedy <me@jeremyk.net>
2025-07-24 21:47:42 -0400

    plaintext password

--- modules/core/base.nix
+++ modules/core/base.nix
@@ -30,8 +30,7 @@
   nixpkgs.config.allowUnfree = true;
 
   # Set root password for console access (KVM)
-  # Using initialHashedPassword - sets password on first boot only
-  users.users.root.initialHashedPassword = "$y$j9T$7tHof6eMlrWk9qrOIjM3m1$Tpfd5r.xgKuSxdFlvDWqJv39gGyS0ceiJt8OSYde1N6"; # "nixos"
+  users.users.root.initialPassword = "securenixos";
 
   # Core system packages are now in packages.nix
 }
